2ab3fb5179316eb77434a49719516715672a137b
14budig <nbudig@gmail.com>
2016-09-08 12:14:50 -0700

    save before pull

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -3,11 +3,11 @@ var db = require('../models');
 function create(req, res){
   var name = req.body.name;
   var desc = req.body.description;
-  ing ingList = req.body.split(',').map(function(item) { return item.trim(); } );
+  var ingList = req.body.split(',').map(function(item) { return item.trim(); } );
   var ingredients = ingList.map(function(item){
     db.Ingredient.findOne({name: item},function(err, foundIngredient){
       if(err){
-          res.status(200).send(err);
+          res.send(err);
       }
       return foundIngredient;
     });
@@ -20,9 +20,8 @@ function create(req, res){
       console.log(recipe);
       res.json(recipe);
     });
-    }
+
   });
- });
 }
 
 module.exports = {
