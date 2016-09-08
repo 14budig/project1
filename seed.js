cb397e121f2173ce5390584149d7fcb00b8469e9
Zachary Fong <fong.zachary@gmail.com>
2016-09-07 19:15:51 -0700

    seed.js working

--- seed.js
+++ seed.js
@@ -22,9 +22,9 @@ db.Ingredient.remove({}, function(err){
   console.log("remove")
 });
 
-db.Ingredient.create(newIngredient, function(err, ingredient){
+db.Ingredient.create(newIngredients, function(err, ingredient){
   if (err){
     console.log("error " + err);
   }
-  console.log("new " + newIngredient);
+  console.log("new " + newIngredients);
 });
